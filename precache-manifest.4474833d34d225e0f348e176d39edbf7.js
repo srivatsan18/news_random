63a57df14d3bf8b22c064967071392a22a758022
srivatsan18 <srivatsan4you@gmail.com>
2020-05-02 21:40:35 +0530

    Updates

---
+++ precache-manifest.4474833d34d225e0f348e176d39edbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd09e4f049ec962f29acd6b3009e52d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa03a8549cbf0d322e6",
+    "url": "/static/css/main.0cead9f7.chunk.css"
+  },
+  {
+    "revision": "37c84a82c2258234b3ec",
+    "url": "/static/js/2.de6e8c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de6e8c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa03a8549cbf0d322e6",
+    "url": "/static/js/main.57b52ab8.chunk.js"
+  },
+  {
+    "revision": "92a755b0cb6ad97329c7",
+    "url": "/static/js/runtime-main.9b080467.js"
+  }
+]);
